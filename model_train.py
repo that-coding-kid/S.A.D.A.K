ac09e108d7d9737b4b4ef56ae06fe312468f603f
ayush <b22153@students.iitmandi.ac.in>
2024-03-21 20:08:53 +0530

    Add model

--- model_train.py
+++ model_train.py
@@ -6,5 +6,5 @@ CUDA_LAUNCH_BLOCKING=1
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="datasets-yolo")
-trainer.setTrainConfig(object_names_array=["potholes","traffic_sign","unknown"], batch_size=32, num_experiments=100, train_from_pretrained_model="yolov3.pt")
+trainer.setTrainConfig(object_names_array=["potholes","traffic_sign","unknown"], batch_size=32, num_experiments=100, train_from_pretrained_model="tiny-yolov3.pt")
 trainer.trainModel()
