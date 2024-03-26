e1f661b733660095ca38cbd809c9d850a4c5aa25
Dev-on-go <divyanshu030904@gmail.com>
2024-03-26 16:55:08 +0530

    Annotated Photos

--- model_train.py
+++ model_train.py
@@ -2,9 +2,10 @@ from imageai.Detection.Custom import DetectionModelTrainer
 import torch
 torch.cuda.empty_cache()
 
-CUDA_LAUNCH_BLOCKING=1
+from imageai.Detection.Custom import DetectionModelTrainer
+
 trainer = DetectionModelTrainer()
-trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="datasets-yolo")
-trainer.setTrainConfig(object_names_array=["potholes","traffic_sign","unknown"], batch_size=32, num_experiments=100, train_from_pretrained_model="yolov3.pt")
+trainer.setModelTypeAsTinyYOLOv3()
+trainer.setDataDirectory(data_directory="/home/dev/Documents/GitHub/S.A.D.A.K/datasets-yolo")
+trainer.setTrainConfig(object_names_array=["Sever Cover", "traffic_sign", "Drain Hole", "Pothole"], batch_size=32, num_experiments=100, train_from_pretrained_model="/home/dev/Documents/GitHub/S.A.D.A.K/tiny-yolov3.pt")
 trainer.trainModel()
